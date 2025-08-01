909229d93d18e4a03361ea4e91472d4173a31b0b
Chun-Yuan Chen <cychen.da@gmail.com>
2025-08-01 18:00:08 -0400

    The assignment for the Shell and Git modules has been completed by Chun-Yuan Chen.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -27,21 +27,89 @@ unzip -q rawdata.zip
 ###########################################
 # Complete assignment here
 
+
+# ==============================================================================
+# Program         : Machine Learning Software Foundations
+# Module          : Shell and Git
+# Cohort          : 7
+# Author          : Chun-Yuan Chen
+# GitHub          : https://github.com/Chun-YuanChen
+# Date Created    : 2025-08-01
+# Last Updated    : 2025-08-01
+# ==============================================================================
+
+
 # 1. Create a directory named data
+mkdir data
+pwd
+ls
 
 # 2. Move the ./rawdata directory to ./data/raw
+mv rawdata data/raw
+ls
+cd data
+ls
 
 # 3. List the contents of the ./data/raw directory
+pwd
+ls
+cd raw
+ls
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+pwd
+cd ..
+pwd
+mkdir processed
+ls
+cd processed
+pwd
+mkdir server_logs user_logs event_logs
+ls
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd ..
+ls
+cd raw
+ls
+cd ..
+ls
+cp raw/*server*.log processed/server_logs/
+cd processed/server_logs
+ls
 
 # 6. Repeat the above step for user logs and event logs
+pwd
+cd ..
+cd ..
+ls
+cp raw/*user*.log processed/user_logs/
+cp raw/*event*.log processed/event_logs/
+cd processed/user_logs/
+ls
+cd ..
+cd ..
+cd processed/event_logs/
+ls
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+pwd
+cd ~/week1/shell/02_activities/assignments/newproject/data/raw/
+ls
+rm ./*ipaddr*
+ls
+cd ~/week1/shell/02_activities/assignments/newproject/data/processed/user_logs
+pwd
+ls
+rm ./*ipaddr*
+ls
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+cd ~/week1/shell/02_activities/assignments/newproject/data/
+ls processed/*
+ls processed/* > inventory.txt
+ls
+code inventory.txt
 
 
 ###########################################
