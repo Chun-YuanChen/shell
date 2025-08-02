6856ac352860ca6f61572edecb5d22dc8e2abb71
Chun-Yuan Chen <cychen.da@gmail.com>
2025-08-01 20:39:04 -0400

    Fixed commands.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -46,12 +46,9 @@ pwd
 
 # 2. Move the ./rawdata directory to ./data/raw
 mv rawdata data/raw
-ls
-cd data
 
 # 3. List the contents of the ./data/raw directory
-pwd
-cd raw
+cd data/raw/
 ls
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
@@ -59,35 +56,26 @@ pwd
 cd ..
 pwd
 mkdir processed
-ls
 cd processed
 pwd
 mkdir server_logs user_logs event_logs
-ls
+
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ..
-ls
 cd raw
-ls
 cd ..
-ls
 cp raw/*server*.log processed/server_logs/
 cd processed/server_logs
-ls
+
 
 # 6. Repeat the above step for user logs and event logs
 pwd
 cd ..
 cd ..
-ls
 cp raw/*user*.log processed/user_logs/
 cp raw/*event*.log processed/event_logs/
-cd processed/user_logs/
-ls
-cd ..
-cd ..
-cd processed/event_logs/
+
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 pwd
